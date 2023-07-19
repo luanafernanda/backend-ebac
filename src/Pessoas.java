518fd923f929cc9eb41976d6063e6d233c8caa14
luanafernanda <lfsg.22@outlook.com>
2023-07-19 15:58:32 -0300

    exercicio pratico com lambda

---
+++ src/Pessoas.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Pessoas {
+    public static void main(String[] args) {
+        List<String> feminino = new ArrayList<>();
+        List<String> masculino = new ArrayList<>();
+
+        Scanner leitor  = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Digite o nome da pessoa (ou 'sair' para encerrar): ");
+            String nome = leitor.nextLine();
+
+            if (nome.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            System.out.print("Digite o sexo da pessoa (F - feminino, M - masculino): ");
+            String sexo = leitor.nextLine();
+
+            if (sexo.equalsIgnoreCase("F")) {
+                feminino.add(nome);
+            } else if (sexo.equalsIgnoreCase("M")) {
+                masculino.add(nome);
+            } else {
+                System.out.println("Sexo inválido! Por favor, digite F ou M.");
+            }
+        }
+
+        System.out.println("Pessoas do grupo feminino:");
+        feminino.forEach(nome -> System.out.println(nome));
+    }
+}
